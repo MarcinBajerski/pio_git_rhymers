2f4ba273d8ad51936084290c51e62b700430347f
marci <245761@edu.p.lodz.pl>
2026-04-21 21:15:18 +0200

    projekt jest poprawny

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public final int STACK_SIZE = 12;
-    public final int EMPTY_STACK_VAL = -1;
-    public final int FULL_STACK_VAL = 11;
+    public static final int STACK_SIZE = 12;
+    public static final int EMPTY_STACK_VAL = -1;
+    public static final int FULL_STACK_VAL = 11;
 
     private int[] numbers = new int[STACK_SIZE];
 
@@ -21,37 +21,17 @@ public class DefaultCountingOutRhymer {
 
     public boolean isFull() {
         return total == FULL_STACK_VAL;
-    private int[] NUMBERS = new int[12];
-
-    public int total = -1;
-
-    public void countIn(int in) {
-        if (!isFull())
-            NUMBERS[++total] = in;
-    }
-
-    public boolean callCheck() {
-        return total == -1;
-    }
-
-    public boolean isFull() {
-        return total == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return EMPTY_STACK_VAL;
         return numbers[total];
-            return -1;
-        return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
             return EMPTY_STACK_VAL;
         return numbers[total--];
-            return -1;
-        return NUMBERS[total--];
     }
-
 }
