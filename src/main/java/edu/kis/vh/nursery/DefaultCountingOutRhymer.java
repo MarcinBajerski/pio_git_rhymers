156031f6b1f7ac46c2dde5babf19c05deb94d2ba
marci <245761@edu.p.lodz.pl>
2026-04-21 21:19:24 +0200

    Zadanie 17: Dokumentacja Javadoc

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa DefaultCountingOutRhymer służy do przechowywania i wyliczania rymowanek przy użyciu tablicy.
+ */
 public class DefaultCountingOutRhymer {
 
     public static final int STACK_SIZE = 12;
@@ -10,25 +13,45 @@ public class DefaultCountingOutRhymer {
 
     public int total = EMPTY_STACK_VAL;
 
+    /**
+     * Dodaje nową wartość na koniec wyliczanki (na wierzch stosu).
+     * * @param in wartość liczbowa, która ma zostać dodana
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka (stos) jest obecnie pusta.
+     * * @return true jeśli wyliczanka jest pusta, w przeciwnym razie false
+     */
     public boolean callCheck() {
         return total == EMPTY_STACK_VAL;
     }
 
+    /**
+     * Sprawdza, czy wyliczanka (stos) osiągnęła maksymalną pojemność i nie przyjmie więcej wartości.
+     * * @return true jeśli wyliczanka jest pełna, w przeciwnym razie false
+     */
     public boolean isFull() {
         return total == FULL_STACK_VAL;
     }
 
+    /**
+     * Zwraca ostatnio dodaną wartość (z wierzchu stosu) bez jej usuwania.
+     * * @return ostatnio dodana liczba lub wartość EMPTY_STACK_VAL, jeśli stos jest pusty
+     */
     protected int peekaboo() {
         if (callCheck())
             return EMPTY_STACK_VAL;
         return numbers[total];
     }
 
+    /**
+     * Pobiera i usuwa ostatnio dodaną wartość z wyliczanki (zdejmuje ze stosu).
+     * * @return zdjęta liczba lub wartość EMPTY_STACK_VAL, jeśli stos jest pusty
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY_STACK_VAL;
